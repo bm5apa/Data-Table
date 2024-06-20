cea6616ef435acf741925bc71ce65ae4c161f41b
bm5apa <timelessheaven@gmail.com>
2024-06-20 14:11:57 +0800

    build: project init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index.hbs')
+})
+
+app.get('/', (req, res) => {
+  res.send('index.hbs')
+})
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`)
+})
